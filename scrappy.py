2b1eac124193db0e7f9d65d9003692e2a2f76f38
Zeph LaFassett <zelafassett@gmail.com>
2021-09-01 10:25:32 -0700

    extracting paragraphs

--- scrappy.py
+++ scrappy.py
@@ -5,17 +5,30 @@ from bs4 import BeautifulSoup
 from lxml import html
 import time
 
-def request_page(href, num):
+def extract_article(soup):
+    '''Extracts the text from the article'''
+    panel = soup.select('div.panel.article')[0]
+    print ()
+    texts = []
+    paragraphs = panel.find_all('p')
+    for p in paragraphs:
+        texts.append(p.text)
+    print(texts)
+    return texts
+
+def process_request(href, num, content):
     '''Gets the next page'''
     link = href + "?page=" + str(num)
     req = requests.get(link, headers=headers)
-    print("%s, %s" % (link, req.status_code))
-    time.sleep(2.5)
     if req.status_code == 200:
+        time.sleep(2.5)
         num += 1
-        request_page(href, num)
+        soup = BeautifulSoup(req.text, 'html.parser')
+        content.append(extract_article(soup))
+        return process_request(href, num, content)
     else:
         print(link + " Gave a " + str(req.status_code))
+    return content
 
 URL = "https://www.literotica.com/c/mature-sex/78-page"
 headers = {
@@ -27,9 +40,10 @@ stories = soup.find_all('div', class_='b-sl-item-r')
 links = []
 for story in stories:
     links.append(story.find('h3').find('a')['href'])
-print(links)
+# print(links)
 
 for link in links:
     print("STARTING===: " + link)
-    request_page(link, 1)
+    content = process_request(link, 1, [])
+    print("FOUND %s Number Article Pages " % len(content))
     print("DONE WITH==: " + link)
